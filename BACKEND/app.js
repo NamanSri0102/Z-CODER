ceb08051ba9beb907df82ea2503e1fe03db84336
Naman Srivastava <139103521+NamanSri0102@users.noreply.github.com>
2024-11-30 06:36:52 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const UserRouting=require('./Routes/User-Routing');
 const bodyParser=require('body-parser');
 const QuestionRouting=require('./Routes/Question-Routing');
